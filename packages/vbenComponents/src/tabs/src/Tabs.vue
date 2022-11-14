1f74755a03b3d127571510088ea0ec00ab5cb66e
JinMao <jinmao88@qq.com>
2022-11-15 07:19:42 +0800

    feat: VbenTabs 增加暴露Ref 便于外部获取

--- packages/vbenComponents/src/tabs/src/Tabs.vue
+++ packages/vbenComponents/src/tabs/src/Tabs.vue
@@ -1,10 +1,13 @@
 <script lang="ts" setup name="VbenTabs">
 import { maps } from '#/index'
+import { ref } from 'vue'
 
 const Tabs = maps.get('Tabs')
+const Ref = ref(null)
+defineExpose({ Ref })
 </script>
 <template>
-  <Tabs v-bind="$attrs">
+  <Tabs v-bind="$attrs" ref="Ref">
     <template #[item]="data" v-for="item in Object.keys($slots)" :key="item">
       <slot :name="item" v-bind="data || {}"></slot> </template
   ></Tabs>

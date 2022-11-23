602204790bc54666edb7c98cbadd6a147a1b36fc
h_mo <596417202@qq.com>
2022-11-23 12:44:08 +0800

    fix: 修复menuSetting属性设置错误

--- apps/admin/src/layout/components/app/AppProvider.tsx
+++ apps/admin/src/layout/components/app/AppProvider.tsx
@@ -1,24 +1,24 @@
-import { createBreakpointListen } from '@/hooks/event/use-breakpoint'
-import { namespace } from '@/setting'
-import { defineComponent, toRefs, ref, unref } from 'vue'
-import { createAppProviderContext } from '@/hooks/web/useAppContext'
-import { useAppStoreWithOut } from '@/store/config'
-import { MenuModeEnum, MenuTypeEnum } from '@vben/constants'
-import { useMsg, useNotice } from '@vben/vbencomponents'
-import { setMsg, setNoice } from '@vben/request'
+import {createBreakpointListen} from '@/hooks/event/use-breakpoint'
+import {namespace} from '@/setting'
+import {defineComponent, toRefs, ref, unref} from 'vue'
+import {createAppProviderContext} from '@/hooks/web/useAppContext'
+import {useAppStoreWithOut} from '@/store/config'
+import {MenuModeEnum, MenuTypeEnum} from '@vben/constants'
+import {useMsg, useNotice} from '@vben/vbencomponents'
+import {setMsg, setNoice} from '@vben/request'
 
 const props = {
   /**
    * class style prefix
    */
-  prefixCls: { type: String, default: namespace },
+  prefixCls: {type: String, default: namespace},
 }
 
 export default defineComponent({
   name: 'AppProvider',
   inheritAttrs: false,
   props,
-  setup(props, { slots }) {
+  setup(props, {slots}) {
     const isMobile = ref(false)
     const isSetState = ref(false)
 
@@ -29,7 +29,7 @@ export default defineComponent({
     setMsg(msg)
     setNoice(notice)
     // Monitor screen breakpoint information changes
-    createBreakpointListen(({ screenMap, sizeEnum, width }) => {
+    createBreakpointListen(({screenMap, sizeEnum, width}) => {
       const lgWidth = screenMap.get(sizeEnum.LG)
       if (lgWidth) {
         isMobile.value = width.value - 1 < lgWidth
@@ -37,10 +37,10 @@ export default defineComponent({
       handleRestoreState()
     })
 
-    const { prefixCls } = toRefs(props)
+    const {prefixCls} = toRefs(props)
 
     // Inject variables into the global
-    createAppProviderContext({ prefixCls, isMobile })
+    createAppProviderContext({prefixCls, isMobile})
 
     /**
      * Used to maintain the state before the window changes
@@ -59,8 +59,8 @@ export default defineComponent({
           } = appStore.getProjectConfig
           appStore.setProjectConfig({
             menuSetting: {
-              type: MenuTypeEnum,
-              mode: MenuModeEnum,
+              type: menuType,
+              mode: menuMode,
               split: false,
             },
           })
@@ -74,8 +74,7 @@ export default defineComponent({
       } else {
         if (unref(isSetState)) {
           isSetState.value = false
-          const { menuMode, menuCollapsed, menuType, menuSplit } =
-            appStore.getBeforeMiniInfo
+          const {menuMode, menuCollapsed, menuType, menuSplit} = appStore.getBeforeMiniInfo
           appStore.setProjectConfig({
             menuSetting: {
               type: menuType,
@@ -87,6 +86,7 @@ export default defineComponent({
         }
       }
     }
+
     return () => slots.default?.()
   },
 })

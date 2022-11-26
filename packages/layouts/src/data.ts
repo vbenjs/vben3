703edb1260366484db6f572e6f0b439e25299cc9
jinmao88 <50581550+jinmao88@users.noreply.github.com>
2022-11-26 11:00:49 +0800

    feat: 重构获取header高度

--- packages/layouts/src/data.ts
+++ packages/layouts/src/data.ts
@@ -1,7 +1,7 @@
-import { computed, ref } from 'vue'
-
+import { computed, ref, unref } from 'vue'
+import { useElementSize } from '@vben/use'
+import { context } from '../bridge'
 export const layoutHeader = ref(null)
 // 获取header高度
-export const layoutHeaderHeight = computed(
-  () => layoutHeader.value?.$el.clientHeight || 0,
-)
+export const headerRef = ref<HTMLElement | null>(null)
+export const { height, width } = useElementSize(headerRef)

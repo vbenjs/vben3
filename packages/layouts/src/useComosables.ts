f61dbb6d52628decd059f5fc085d43460fc93c86
h_mo <596417202@qq.com>
2023-01-06 23:15:04 +0800

    fix: 修复固定头部

--- packages/layouts/src/useComosables.ts
+++ packages/layouts/src/useComosables.ts
@@ -1,18 +1,22 @@
-import {computed, onMounted, ref, unref} from 'vue'
-import { useElementSize } from '@vben/utils'
+import {computed, ref, unref} from 'vue'
+import {useElementSize} from '@vben/utils'
+import {context} from '../bridge'
 
-export const useComosables = ()=>{
+export const useComosables = () => {
+  const {useHeaderSetting} = context
+  // @ts-ignore
+  const {getFixed: getHeaderFixed} = useHeaderSetting();
   const headerRef = ref<HTMLElement | null>(null)
   const tabRef = ref<HTMLElement | null>(null)
-  const { height: headerHeight, width: headerWidth } = useElementSize(headerRef)
-  const { height: tabHeight, width: tabWidth } = useElementSize(tabRef)
-  const omitContentHeight = computed(()=>{
+  const {height: headerHeight, width: headerWidth} = useElementSize(headerRef)
+  const {height: tabHeight, width: tabWidth} = useElementSize(tabRef)
+  const omitContentHeight = computed(() => {
     return unref(headerHeight) + unref(tabHeight)
   })
-  const contentHeight = computed(()=>{
-    return `calc(100vh - ${unref(omitContentHeight)}px)`
+  const contentHeight = computed(() => {
+    return unref(getHeaderFixed) ? `calc(100vh - ${unref(omitContentHeight)}px)` : 'auto'
   })
-  const contentStyle = computed(()=>{
+  const contentStyle = computed(() => {
     return {
       height: unref(contentHeight),
       minHeight: unref(contentHeight)

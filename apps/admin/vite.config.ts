ad93b945f44cc87737b86aed8448ba6978fb7dd9
Kirk Lin <linkirk@163.com>
2023-03-20 11:37:06 +0800

    feat: add loadRoutesFromModules (#61)

--- apps/admin/vite.config.ts
+++ apps/admin/vite.config.ts
@@ -2,6 +2,5 @@ import { createViteConfig } from '@config/vite'
 import { defineConfig, UserConfig } from 'vite'
 
 export default defineConfig(async ({ command, mode }): Promise<UserConfig> => {
-  const cfg = await createViteConfig(command, mode, process.cwd(), { preset: 'antd' })
-  return cfg
+  return await createViteConfig(command, mode, process.cwd(), {preset: 'antd'})
 })

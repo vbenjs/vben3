c666a65dcdd61ab532937bf632174149c3312f11
RandyZhang <zhangshi3421@gmail.com>
2023-06-12 20:32:05 +0800

    补充单元测试

--- configs/vite/test/plugins/monorepo.test.ts
+++ configs/vite/test/plugins/monorepo.test.ts
@@ -1,9 +1,8 @@
 import { expect, test, describe } from 'vitest';
 import path from 'path';
-import { createAlias } from '../../src/plugins/monorepo';
+import { createAlias, findNearestPackageData } from '../../src/plugins/monorepo';
 import { ResolverFunction } from 'vite';
 
-const TEST_TITLE = '【测试MonoRepoResolverPlugin】'
 const buildTestPkgPath = (pkgRoot: string = process.cwd()) => {
   const demoPkgPath = path.resolve(pkgRoot, './configs/vite/test/plugins/testpkg')
   return [
@@ -11,6 +10,14 @@ const buildTestPkgPath = (pkgRoot: string = process.cwd()) => {
     path.resolve(demoPkgPath, './index.ts')
   ]
 }
+
+describe('findNearestPackageData无法获取包', async () => {
+  test('findNearestPackageData无法获取包', () => {
+    const importerId = path.resolve('/', './not_exist')
+    const ret = findNearestPackageData(importerId);
+    expect(ret).toBeNull()
+  })
+})
 
 describe('【测试MonoRepoResolverPlugin】', () => {
   test(`正常创建alias`, async () => {
@@ -29,18 +36,46 @@ describe('【测试MonoRepoResolverPlugin】', () => {
   const customResolver = aliasConfig.customResolver as ResolverFunction
 
   describe('测试customResolver解析组件路径', () => {
-    test(`正常解析testpkg/src/test.ts`, async () => {
+    test(`正常解析testpkg/src/test的形式`, async () => {
       const [, importerId] = buildTestPkgPath()
       expect(customResolver.call(null, 'src/test', importerId, null)).contains('test/plugins/testpkg/src/test.ts')
+    })
+
+    test(`正常解析testpkg/src/test.ts的形式`, async () => {
+      const [, importerId] = buildTestPkgPath()
+      expect(customResolver.call(null, 'src/test.ts', importerId, null)).contains('test/plugins/testpkg/src/test.ts')
+    })
+
+    test(`组件中存在index.ts和index.vue, 解析到index.ts`, async () => {
+      const [, importerId] = buildTestPkgPath()
+      expect(customResolver.call(null, 'src/vue&ts', importerId)).contains('testpkg/src/vue&ts/index.ts')
+    })
+
+    test('单个Vue文件的解析', async () => {
+      const [, importerId] = buildTestPkgPath()
+      expect(customResolver.call(null, 'src/SingleVue', importerId)).contains('test/plugins/testpkg/src/SingleVue.vue')
+    })
+    
+    test('文件夹下index.vue文件的解析', async () => {
+      const [, importerId] = buildTestPkgPath()
+      expect(customResolver.call(null, 'src/justvue', importerId)).contains('test/plugins/testpkg/src/justvue/index.vue')
     })
     
     test(`找不到组件`, async () => {
       const [, importerId] = buildTestPkgPath()
       expect(() => customResolver.call(null, 'src/notfound', importerId)).toThrowError(/find none files at/)
     })
-    
-    test(`组件中存在index.ts和index.vue, 解析到index.ts`, async () => {
+
+    test(`找到不认识格式的组件`, async () => {
       const [, importerId] = buildTestPkgPath()
+      expect(() => customResolver.call(null, 'src/notype', importerId)).toThrowError(/\/index.\(ts\|js\), please check it./)
+    })
+})
+
+  describe('测试包内互相引用', () => {
+    test('测试test.ts中解析vue&ts的index', () => {
+      const [demoPkgPath] = buildTestPkgPath()
+      const importerId = path.resolve(demoPkgPath, './src/test.ts')
       expect(customResolver.call(null, 'src/vue&ts', importerId)).contains('testpkg/src/vue&ts/index.ts')
     })
   })

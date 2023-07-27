a8fb1079be369e8119fff87be5f4678a88cc9456
ya2glu <ya2glu@163.com>
2023-07-27 17:42:14 +0800

    ➕[comp]:新增Alert组件

---
+++ packages/vbenComponents/src/alert/src/Alert.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup name="VbenAlert">
+import {maps} from '#/index'
+
+const Alert = maps.get('Alert')
+</script>
+<template>
+  <div>
+    <Alert v-bind="$attrs">
+      <template #[item]="data" v-for="item in Object.keys($slots)" :key="item">
+        <slot :name="item" v-bind="data || {}"></slot>
+      </template>
+    </Alert>
+  </div>
+</template>
+
+<style lang="less" scoped></style>

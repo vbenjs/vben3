e753316650abebfcb71c2d6465b774a3ed8745da
Li Kui <90845831+likui628@users.noreply.github.com>
2023-09-10 13:38:50 +0800

    chore: format code

--- packages/layouts/src/components/setting/components/Content.vue
+++ packages/layouts/src/components/setting/components/Content.vue
@@ -1,10 +1,10 @@
 <script lang="ts" setup>
 import SwitchItem from './SwitchItem.vue'
-import {HandlerSettingEnum} from '@vben/constants'
-import {context} from '../../../../bridge'
-import {useI18n} from '@vben/locale'
+import { HandlerSettingEnum } from '@vben/constants'
+import { context } from '../../../../bridge'
+import { useI18n } from '@vben/locale'
 
-const {t} = useI18n();
+const { t } = useI18n()
 
 const {
   useMultipleTabSetting,
@@ -21,52 +21,88 @@ const {
   getFullContent,
   getColorWeak,
   getGrayMode,
-} = useRootSetting();
+} = useRootSetting()
 
+const { getIsHorizontal, getShowMenu, getIsMixSidebar } = useMenuSetting()
 
-const {
-  getIsHorizontal,
-  getShowMenu,
-  getIsMixSidebar,
-} = useMenuSetting();
+const { getShowHeader } = useHeaderSetting()
 
-const {getShowHeader} = useHeaderSetting();
-
-const {getShowMultipleTab, getShowQuick, getShowRedo, getShowFold} = useMultipleTabSetting();
-
+const { getShowMultipleTab, getShowQuick, getShowRedo, getShowFold } =
+  useMultipleTabSetting()
 </script>
 <template>
   <VbenSpace vertical>
-    <SwitchItem :title="t('layout.setting.breadcrumb')" :def="getShowBreadCrumb"
-                :event="HandlerSettingEnum.SHOW_BREADCRUMB"
-                :disabled="!getShowHeader"/>
-    <SwitchItem :title="t('layout.setting.breadcrumbIcon')" :def="getShowBreadCrumbIcon"
-                :event="HandlerSettingEnum.SHOW_BREADCRUMB_ICON" :disabled="!getShowBreadCrumb"/>
-    <SwitchItem :title="t('layout.setting.tabs')" :def="getShowMultipleTab"
-                :event="HandlerSettingEnum.TABS_SHOW"/>
-    <SwitchItem :title="t('layout.setting.tabsRedoBtn')" :def="getShowRedo"
-                :event="HandlerSettingEnum.TABS_SHOW_REDO"
-                :disabled="!getShowMultipleTab"/>
-    <SwitchItem :title="t('layout.setting.tabsQuickBtn')" :def="getShowQuick"
-                :event="HandlerSettingEnum.TABS_SHOW_QUICK"
-                :disabled="!getShowMultipleTab"/>
-    <SwitchItem :title="t('layout.setting.tabsFoldBtn')" :def="getShowFold"
-                :event="HandlerSettingEnum.TABS_SHOW_FOLD"
-                :disabled="!getShowMultipleTab"/>
-    <SwitchItem :title="t('layout.setting.sidebar')" :def="getShowMenu"
-                :event="HandlerSettingEnum.MENU_SHOW_SIDEBAR"
-                :disabled="getIsHorizontal"/>
-    <SwitchItem :title="t('layout.setting.header')" :def="getShowHeader"
-                :event="HandlerSettingEnum.HEADER_SHOW"/>
-    <SwitchItem title="Logo" :def="getShowLogo" :event="HandlerSettingEnum.SHOW_LOGO"
-                :disabled="getIsMixSidebar"/>
-    <SwitchItem :title="t('layout.setting.footer')" :def="getShowFooter"
-                :event="HandlerSettingEnum.SHOW_FOOTER"/>
-    <SwitchItem :title="t('layout.setting.fullContent')" :def="getFullContent"
-                :event="HandlerSettingEnum.FULL_CONTENT"/>
-    <SwitchItem :title="t('layout.setting.grayMode')" :def="getGrayMode"
-                :event="HandlerSettingEnum.GRAY_MODE"/>
-    <SwitchItem :title="t('layout.setting.colorWeak')" :def="getColorWeak"
-                :event="HandlerSettingEnum.COLOR_WEAK"/>
+    <SwitchItem
+      :title="t('layout.setting.breadcrumb')"
+      :def="getShowBreadCrumb"
+      :event="HandlerSettingEnum.SHOW_BREADCRUMB"
+      :disabled="!getShowHeader"
+    />
+    <SwitchItem
+      :title="t('layout.setting.breadcrumbIcon')"
+      :def="getShowBreadCrumbIcon"
+      :event="HandlerSettingEnum.SHOW_BREADCRUMB_ICON"
+      :disabled="!getShowBreadCrumb"
+    />
+    <SwitchItem
+      :title="t('layout.setting.tabs')"
+      :def="getShowMultipleTab"
+      :event="HandlerSettingEnum.TABS_SHOW"
+    />
+    <SwitchItem
+      :title="t('layout.setting.tabsRedoBtn')"
+      :def="getShowRedo"
+      :event="HandlerSettingEnum.TABS_SHOW_REDO"
+      :disabled="!getShowMultipleTab"
+    />
+    <SwitchItem
+      :title="t('layout.setting.tabsQuickBtn')"
+      :def="getShowQuick"
+      :event="HandlerSettingEnum.TABS_SHOW_QUICK"
+      :disabled="!getShowMultipleTab"
+    />
+    <SwitchItem
+      :title="t('layout.setting.tabsFoldBtn')"
+      :def="getShowFold"
+      :event="HandlerSettingEnum.TABS_SHOW_FOLD"
+      :disabled="!getShowMultipleTab"
+    />
+    <SwitchItem
+      :title="t('layout.setting.sidebar')"
+      :def="getShowMenu"
+      :event="HandlerSettingEnum.MENU_SHOW_SIDEBAR"
+      :disabled="getIsHorizontal"
+    />
+    <SwitchItem
+      :title="t('layout.setting.header')"
+      :def="getShowHeader"
+      :event="HandlerSettingEnum.HEADER_SHOW"
+    />
+    <SwitchItem
+      title="Logo"
+      :def="getShowLogo"
+      :event="HandlerSettingEnum.SHOW_LOGO"
+      :disabled="getIsMixSidebar"
+    />
+    <SwitchItem
+      :title="t('layout.setting.footer')"
+      :def="getShowFooter"
+      :event="HandlerSettingEnum.SHOW_FOOTER"
+    />
+    <SwitchItem
+      :title="t('layout.setting.fullContent')"
+      :def="getFullContent"
+      :event="HandlerSettingEnum.FULL_CONTENT"
+    />
+    <SwitchItem
+      :title="t('layout.setting.grayMode')"
+      :def="getGrayMode"
+      :event="HandlerSettingEnum.GRAY_MODE"
+    />
+    <SwitchItem
+      :title="t('layout.setting.colorWeak')"
+      :def="getColorWeak"
+      :event="HandlerSettingEnum.COLOR_WEAK"
+    />
   </VbenSpace>
 </template>
--- packages/layouts/src/components/setting/components/NavigationBarPicker.vue
+++ packages/layouts/src/components/setting/components/NavigationBarPicker.vue
@@ -1,11 +1,15 @@
 <script lang="ts" setup name="NavigationBarPicker">
-import {useI18n} from '@vben/locale'
+import { MenuModeEnum, MenuTypeEnum } from '@vben/constants'
+import { useI18n } from '@vben/locale'
 
-const {t} = useI18n();
-defineProps({
+const { t } = useI18n()
+
+const props = defineProps({
   typeList: {
-    type: Array as PropType<object[]>,
-    default: ()=> []
+    type: Array as PropType<
+      { title: string; type: MenuTypeEnum; mode: MenuModeEnum }[]
+    >,
+    default: () => [],
   },
   def: {
     type: String,
@@ -13,102 +17,98 @@ defineProps({
   },
 })
 
-const emits = defineEmits(['handler']);
+const emits = defineEmits(['handler'])
 
-const handlerPicker = (data)=>{
+const handlerPicker = (data) => {
   emits('handler', data)
 }
-
-
 </script>
 <template>
   <div class="setting-navigation-bar-picker">
     <VbenSpace justify="space-between">
-      <template v-for="(item, index) in typeList" :key="index">
-        <VbenTooltip
-          placement="bottom"
-          trigger="hover">
+      <template v-for="(item, index) in props.typeList" :key="index">
+        <VbenTooltip placement="bottom" trigger="hover">
           <template #trigger>
-            <div class="h-48px w-56px bg-gray-300 rounded relative
-                        shadow-inner box-border cursor-pointer
-                        overflow-hidden bar-item"
-                 :class="[(`bar-item__${item.type}`), {active: item.type == def}]"
-                 @click="handlerPicker(item)">
+            <div
+              class="h-48px w-56px bg-gray-300 rounded relative shadow-inner box-border cursor-pointer overflow-hidden bar-item"
+              :class="[`bar-item__${item.type}`, { active: item.type == def }]"
+              @click="handlerPicker(item)"
+            >
               <div class="mix-sidebar"></div>
             </div>
           </template>
           <span>{{ t(item.title) }}</span>
         </VbenTooltip>
       </template>
-
     </VbenSpace>
   </div>
 </template>
 <style lang="less" scoped>
-.setting-navigation-bar-picker{
-  .bar-item{
-    box-shadow: 0px 1px 5px rgba(156,163,173,1);
-    &:hover,&.active{
-      border: 2px solid rgba(6,96,189,1);
+.setting-navigation-bar-picker {
+  .bar-item {
+    box-shadow: 0px 1px 5px rgba(156, 163, 173, 1);
+    &:hover,
+    &.active {
+      border: 2px solid rgba(6, 96, 189, 1);
     }
-    &:before, &:after{
+    &:before,
+    &:after {
       content: '';
       display: inline-block;
       position: absolute;
       top: 0;
       left: 0;
     }
-    &:before{
+    &:before {
       width: 33%;
       height: 100%;
       z-index: 1;
-      background-color: rgba(12,33,53,1);
+      background-color: rgba(12, 33, 53, 1);
     }
-    &:after{
+    &:after {
       width: 100%;
       height: 25%;
-      background-color: rgba(249,250,251,1);
+      background-color: rgba(249, 250, 251, 1);
     }
   }
-  .bar-item__sidebar{
-    &:before{
+  .bar-item__sidebar {
+    &:before {
       z-index: 1;
     }
   }
-  .bar-item__mix{
-    &:before{
+  .bar-item__mix {
+    &:before {
       width: 33%;
       height: 100%;
-      background-color: rgba(249,250,251,1);
+      background-color: rgba(249, 250, 251, 1);
     }
-    &:after{
+    &:after {
       z-index: 1;
-      background-color: rgba(12,33,53,1);
+      background-color: rgba(12, 33, 53, 1);
     }
   }
-  .bar-item__top-menu{
-    &:before{
+  .bar-item__top-menu {
+    &:before {
       display: none;
     }
-    &:after{
+    &:after {
       z-index: 1;
       width: 100%;
       height: 25%;
-      background-color: rgba(12,33,53,1);
+      background-color: rgba(12, 33, 53, 1);
     }
   }
-  .bar-item__mix-sidebar{
-    &:before{
+  .bar-item__mix-sidebar {
+    &:before {
       width: 25%;
       z-index: 1;
     }
-    .mix-sidebar{
+    .mix-sidebar {
       width: 18%;
       height: 100%;
-      background-color: rgba(249,250,251,1);
+      background-color: rgba(249, 250, 251, 1);
       margin-left: 25%;
     }
   }
 }
-
 </style>

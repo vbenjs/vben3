49a610049570908ab648749cf776b7ed03021a7c
Li Kui <90845831+likui628@users.noreply.github.com>
2023-09-17 21:38:22 +0800

    i18n with pinia store
    see https://stackoverflow.com/questions/69212719

--- packages/stores/src/modules/siteGeneral.ts
+++ packages/stores/src/modules/siteGeneral.ts
@@ -1,13 +1,11 @@
 import { defineStore } from 'pinia'
 import { FooterLinkOptions, DefineSiteGeneralOptions } from '@vben/types'
-import { useI18n } from '@vben/locale'
+import { i18n } from '@vben/locale'
 // @ts-ignore
 import defaultLogo from '@/assets/images/logo.png'
 // @ts-ignore
 import defaultAvatar from '@/assets/images/header.jpg'
 import { getGlobalConfig } from '@vben/utils'
-
-const { t } = useI18n()
 
 // @ts-ignore
 const { title } = getGlobalConfig(import.meta.env)
@@ -21,28 +19,26 @@ export const DOC_URL = 'https://vbenjs.github.io/vben3-doc/'
 // site url
 export const SITE_URL = 'https://vben.mufei88.com/'
 
-const links: FooterLinkOptions[] = [
-  {
-    label: t('layout.footer.onlinePreview'),
-    url: SITE_URL,
-  },
-  {
-    icon: 'uim:github',
-    url: GITHUB_URL,
-  },
-  {
-    label: t('layout.footer.onlineDocument'),
-    url: DOC_URL,
-  },
-]
-
 export const useSiteGeneral = defineStore({
   id: 'APP_SITE_GENERAL_OPTIONS',
   state: (): DefineSiteGeneralOptions => ({
     logo: defaultLogo,
     title,
     copyright: '2023 Vben Admin',
-    links: links,
+    links: [
+      {
+        label: i18n.global.t('layout.footer.onlinePreview'),
+        url: SITE_URL,
+      },
+      {
+        icon: 'uim:github',
+        url: GITHUB_URL,
+      },
+      {
+        label: i18n.global.t('layout.footer.onlineDocument'),
+        url: DOC_URL,
+      },
+    ] as FooterLinkOptions[],
     avatar: defaultAvatar,
     username: 'Vben Admin',
   }),

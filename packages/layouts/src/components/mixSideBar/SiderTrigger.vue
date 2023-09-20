3d0809a3dcc1c3318184773210d2bbce3628d832
Li Kui <90845831+likui628@users.noreply.github.com>
2023-09-20 19:53:41 +0800

    chore: sider trigger style

--- packages/layouts/src/components/mixSideBar/SiderTrigger.vue
+++ packages/layouts/src/components/mixSideBar/SiderTrigger.vue
@@ -1,12 +1,11 @@
 <script lang="ts" setup>
-import {context} from "../../../bridge";
-import {createNamespace} from "@vben/utils";
+import { context } from '../../../bridge'
+import { createNamespace } from '@vben/utils'
 
 const { useMenuSetting } = context
 const { toggleCollapsed, getCollapsed } = useMenuSetting()
 
 const { bem } = createNamespace('side-menu')
-
 </script>
 <template>
   <div :class="bem('trigger')" @click.stop="toggleCollapsed">
@@ -16,22 +15,19 @@ const { bem } = createNamespace('side-menu')
 </template>
 
 <style lang="less" scoped>
-.side-menu{
-  &__trigger{
+.side-menu {
+  &__trigger {
     position: absolute;
     bottom: 0;
     left: 0;
     width: 100%;
     font-size: 14px;
-    color: rgb(255 255 255 / 65%);
     text-align: center;
     cursor: pointer;
-    background-color: rgba(59, 59, 59, 1);
     height: 36px;
     display: flex;
     justify-content: center;
     align-items: center;
   }
 }
-
 </style>

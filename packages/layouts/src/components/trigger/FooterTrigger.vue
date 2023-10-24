38779419be4855df086f7036de8c59de81f7f893
afe1 <2279948211@qq.com>
2023-10-24 17:35:42 +0800

    1

--- packages/layouts/src/components/trigger/FooterTrigger.vue
+++ packages/layouts/src/components/trigger/FooterTrigger.vue
@@ -9,16 +9,23 @@ const { bem } = createNamespace('footer-trigger')
 </script>
 <template>
   <span :class="bem()" @click="toggleCollapsed">
-    <VbenIconify v-if="getCollapsed" icon="ant-design:menu-unfold-outlined" />
-    <VbenIconify v-else icon="ant-design:menu-fold-outlined" />
+    <VbenIconify v-if="getCollapsed" icon="ph:caret-double-right-bold" />
+    <VbenIconify v-else icon="ph:caret-double-left-bold" />
   </span>
 </template>
 
 <style lang="less" scoped>
 .footer-trigger {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  font-size: 14px;
+  text-align: center;
+  cursor: pointer;
+  height: 36px;
   display: flex;
   justify-content: center;
   align-items: center;
-  padding: 0 4px;
 }
 </style>

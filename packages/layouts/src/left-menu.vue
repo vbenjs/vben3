070aedf76a7faaa2fd94d6b8165e1d398d7f43e1
afe1 <2279948211@qq.com>
2023-10-24 16:31:05 +0800

    fix: bug122

--- packages/layouts/src/left-menu.vue
+++ packages/layouts/src/left-menu.vue
@@ -4,6 +4,7 @@ import LayoutMenu from './components/menu/index.vue'
 import LayoutHeader from './components/header.vue'
 import LayoutMain from './components/main.vue'
 import LayoutFooter from './components/footer.vue'
+import HeaderTrigger from './components/trigger/FooterTrigger.vue'
 import { context } from '../bridge'
 
 const { useAppConfig, useMenuSetting } = context
@@ -33,7 +34,7 @@ const { toggleCollapse, sidebar, footer } = useAppConfig()
     >
       <slot name="sider">
         <LayoutMenu />
-        <div v-if="getShowFooterTrigger">trigger</div>
+        <HeaderTrigger v-if="getShowFooterTrigger" />
       </slot>
     </VbenLayoutSider>
     <VbenLayout>

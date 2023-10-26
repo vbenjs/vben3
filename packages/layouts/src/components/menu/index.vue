d02ab79b5ddf6282ee3db6e5c9d06bbd0cb437ea
afe1 <56245609+pzzyf@users.noreply.github.com>
2023-10-26 09:46:46 +0800

    fix: “菜单折叠按钮”设置无效(fix #122) (#202)
    * fix: test pr and fix the readme.md content error and style
    
    * fix: bug122
    
    * fix: bug122
    
    * 1
    
    * chore: merge & refactor
    
    * fix: top-menu-mixed trigger
    
    * chore: update menu
    
    ---------
    
    Co-authored-by: Li Kui <90845831+likui628@users.noreply.github.com>

--- packages/layouts/src/components/menu/index.vue
+++ packages/layouts/src/components/menu/index.vue
@@ -14,13 +14,13 @@ import type { RouteMeta } from 'vue-router'
 
 const { Logo, useAppInject, useAppConfig, useMenuSetting } = context
 import { getMenus, listenerRouteChange } from '@vben/router'
+import FooterTrigger from '../trigger/FooterTrigger.vue'
 
 const { getIsMobile } = useAppInject()
 
 const { menu, isMixSidebar, getCollapsedShowTitle, sidebar, isSidebar } =
   useAppConfig()
-const { getTopMenuAlign, getShowFooterTrigger, toggleCollapsed, getCollapsed } =
-  useMenuSetting()
+const { getTopMenuAlign, getShowFooterTrigger } = useMenuSetting()
 const showSidebarLogo = computed(() => {
   return unref(isSidebar) || unref(isMixSidebar)
 })
@@ -126,27 +126,7 @@ const routerToMenu = (item: RouteRecordItem & RouteMeta) => {
         :accordion="menu.accordion"
       />
     </VbenScrollbar>
-    <div v-if="getShowFooterTrigger">
-      <div
-        class="bg-gray-500 h-6 flex justify-center items-center"
-        @click="toggleCollapsed"
-      >
-        <svg
-          focusable="false"
-          :class="getCollapsed ? 'rotate-180' : ''"
-          data-icon="double-left"
-          width="1em"
-          height="1em"
-          fill="currentColor"
-          aria-hidden="true"
-          viewBox="64 64 896 896"
-        >
-          <path
-            d="M272.9 512l265.4-339.1c4.1-5.2.4-12.9-6.3-12.9h-77.3c-4.9 0-9.6 2.3-12.6 6.1L186.8 492.3a31.99 31.99 0 000 39.5l255.3 326.1c3 3.9 7.7 6.1 12.6 6.1H532c6.7 0 10.4-7.7 6.3-12.9L272.9 512zm304 0l265.4-339.1c4.1-5.2.4-12.9-6.3-12.9h-77.3c-4.9 0-9.6 2.3-12.6 6.1L490.8 492.3a31.99 31.99 0 000 39.5l255.3 326.1c3 3.9 7.7 6.1 12.6 6.1H836c6.7 0 10.4-7.7 6.3-12.9L576.9 512z"
-          ></path>
-        </svg>
-      </div>
-    </div>
+    <FooterTrigger v-if="getShowFooterTrigger" />
   </div>
 </template>
 <style>

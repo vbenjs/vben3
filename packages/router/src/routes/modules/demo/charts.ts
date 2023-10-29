9e5a690aa6f4a631ec0a5b6ca73087fea53106ea
likui628 <90845831+likui628@users.noreply.github.com>
2023-10-29 11:27:49 +0800

    fix: build fail

--- packages/router/src/routes/modules/demo/charts.ts
+++ packages/router/src/routes/modules/demo/charts.ts
@@ -1,21 +1,5 @@
 import {LAYOUT} from '../../basic'
 
-// const charts: RouteRecordItem = {
-//   path: '/charts',
-//   name: 'Charts',
-//   component: LAYOUT,
-//   redirect: '/charts/index',
-//   meta: {
-//     title: '图表',
-//     orderNo: 6,
-//     icon: 'material-symbols:bubble-chart-outline',
-//     root: true
-//   },
-//   children: []
-// }
-//
-// export default charts
-
 const charts: RouteRecordItem = {
   path: '/charts',
   name: 'Charts',

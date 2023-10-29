966918abf5c7e243dae894ee499f6397a6c53771
likui628 <90845831+likui628@users.noreply.github.com>
2023-10-29 12:06:25 +0800

    fix: <AMap JSAPI> INVALID_USER_DOMAIN

--- packages/demo/src/Charts/Gaode.vue
+++ packages/demo/src/Charts/Gaode.vue
@@ -5,8 +5,13 @@ import AMapLoader from "@amap/amap-jsapi-loader";
 let map = null;
 
 onMounted(() => {
+  // 您在2021年12月02日以后申请的 key 需要配合您的安全密钥一起使用。
+  window._AMapSecurityConfig = {
+    securityJsCode: "e6cdf70d6db73a22e948ba4addafdddf",
+  }
+
   AMapLoader.load({
-    key: "d7bb98e7185300250dd5f918c12f484b", // 申请好的Web端开发者Key，首次调用 load 时必填
+    key: "b3b5ff64593dae7b68d3daef978dbe3a", // 申请好的Web端开发者Key，首次调用 load 时必填
     version: "2.0", // 指定要加载的 JSAPI 的版本，缺省时默认为 1.4.15
     plugins: ['AMap.Scale'], // 需要使用的的插件列表，如比例尺'AMap.Scale'等
   })

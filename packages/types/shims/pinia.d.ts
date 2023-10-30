84e7fdfdfa83060049f5e88021b1423ad82588cb
王云海 <adoin@qq.com>
2023-10-30 09:56:27 +0800

    types: fix pinia global shim

--- packages/types/shims/pinia.d.ts
+++ packages/types/shims/pinia.d.ts
@@ -3,6 +3,42 @@
 import type { PiniaPluginContext } from 'pinia'
 
 declare module 'pinia' {
+  export interface PersistedStateOptions {
+    /**
+     * Storage key to use.
+     * @default $store.id
+     */
+    key?: string | ((id: string) => string);
+    /**
+     * Where to store persisted state.
+     * @default localStorage
+     */
+    storage?: StorageLike;
+    /**
+     * Dot-notation paths to partially save state. Saves everything if undefined.
+     * @default undefined
+     */
+    paths?: Array<string>;
+    /**
+     * Customer serializer to serialize/deserialize state.
+     */
+    serializer?: Serializer;
+    /**
+     * Hook called before state is hydrated from storage.
+     * @default null
+     */
+    beforeRestore?: (context: PiniaPluginContext) => void;
+    /**
+     * Hook called after state is hydrated from storage.
+     * @default undefined
+     */
+    afterRestore?: (context: PiniaPluginContext) => void;
+    /**
+     * Logs errors in console when enabled.
+     * @default false
+     */
+    debug?: boolean;
+  }
   export interface DefineStoreOptions<
     // eslint-disable-next-line
     Id,
@@ -16,16 +52,6 @@ declare module 'pinia' {
     /**
      * Persist store in storage.
      */
-    persist?: PersistOptions
+    persist?: boolean | PersistedStateOptions | PersistedStateOptions[];
   }
-}
-
-export interface PersistStrategy {
-  key?: string
-  storage?: Storage
-  paths?: string[]
-}
-
-export interface PersistOptions {
-  strategies?: PersistStrategy[]
 }

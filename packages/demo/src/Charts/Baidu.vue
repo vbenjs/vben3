2b0aed3c0db65aeff236f6048a8f3d474252f88a
likui628 <90845831+likui628@users.noreply.github.com>
2023-11-02 19:33:38 +0800

    chore: format code

--- packages/demo/src/Charts/Baidu.vue
+++ packages/demo/src/Charts/Baidu.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import { ref, unref, onMounted } from 'vue';
+import { ref, unref, onMounted } from 'vue'
 import { useDebounceFn } from '@vben/utils'
 import { useScript } from '@vben/hooks'
 defineProps({
@@ -10,34 +10,35 @@ defineProps({
   height: {
     type: String,
     default: 'calc(100vh - 90px)',
-  }
+  },
 })
 const emit = defineEmits(['change-point', 'point-changed'])
-const wrapRef = ref<HTMLDivElement | null>(null);
+const wrapRef = ref<HTMLDivElement | null>(null)
 const map = ref()
 const BMap = ref()
 const geo = ref()
-const BAI_DU_MAP_URL = 'https://api.map.baidu.com/api?v=3.0&ak=qiDNjcA9vYIY7HhzebKkWue4lpSR5z9M&callback=initialize';
-const { toPromise } = useScript({ src: BAI_DU_MAP_URL }, true);
+const BAI_DU_MAP_URL =
+  'https://api.map.baidu.com/api?v=3.0&ak=qiDNjcA9vYIY7HhzebKkWue4lpSR5z9M&callback=initialize'
+const { load } = useScript({ src: BAI_DU_MAP_URL })
 const initBaiduMap = async () => {
-  await toPromise();
+  await load()
   setTimeout(() => {
-    const wrapEl = unref(wrapRef);
-    if (!wrapEl) return;
-    BMap.value = (window as any).BMap;
-    map.value = new BMap.value.Map(wrapEl);
-    geo.value = new BMap.value.Geocoder();
-    const point = new BMap.value.Point(120.373487, 31.508123);
-    map.value.centerAndZoom(point, 15);
-    map.value.enableScrollWheelZoom(true);
+    const wrapEl = unref(wrapRef)
+    if (!wrapEl) return
+    BMap.value = (window as any).BMap
+    map.value = new BMap.value.Map(wrapEl)
+    geo.value = new BMap.value.Geocoder()
+    const point = new BMap.value.Point(120.373487, 31.508123)
+    map.value.centerAndZoom(point, 15)
+    map.value.enableScrollWheelZoom(true)
     map.value.addEventListener('click', function (e) {
-      map.value.clearOverlays();
-      const pt = e.point;
-      const marker = new BMap.value.Marker(new BMap.value.Point(pt.lng, pt.lat));
-      map.value.addOverlay(marker);
+      map.value.clearOverlays()
+      const pt = e.point
+      const marker = new BMap.value.Marker(new BMap.value.Point(pt.lng, pt.lat))
+      map.value.addOverlay(marker)
       latlng2text(pt)
       emit('change-point', e.point)
-    });
+    })
   }, 100)
 }
 // 地理坐标转化成具体的地理位置
@@ -53,22 +54,26 @@ const latlng2text = (point) => {
 const handleInput = useDebounceFn((value: string) => {
   geo.value.getPoint(value, (point) => {
     if (point) {
-      map.value.centerAndZoom(point, 16);
-    }else{
+      map.value.centerAndZoom(point, 16)
+    } else {
       console.log('Geo can not find this place')
     }
   })
-}, 500);
+}, 500)
 onMounted(() => {
-  initBaiduMap();
-});
+  initBaiduMap()
+})
 </script>
 
 <template>
   <div class="bmap-input">
     <VbenInput clearable placeholder="search" @input="handleInput">
       <template #prefix>
-        <VbenIconify :size="20" color="gray" icon="ant-design:search-outlined" />
+        <VbenIconify
+          :size="20"
+          color="gray"
+          icon="ant-design:search-outlined"
+        />
       </template>
     </VbenInput>
   </div>

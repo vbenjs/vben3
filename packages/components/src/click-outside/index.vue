41e1d2f32ea5a165cd3bcbb609e836c1d978a8a1
huangxiaomin <1219549841@qq.com>
2023-11-07 17:00:35 +0800

    perf: delete duplicate references

--- packages/components/src/click-outside/index.vue
+++ packages/components/src/click-outside/index.vue
@@ -1,9 +1,9 @@
 <script lang="ts" setup>
 import { ref, onMounted } from 'vue'
-import { onClickOutside } from '@vueuse/core'
+import { onClickOutside } from '@vben/utils'
 
 const emit = defineEmits(['mounted', 'clickOutside'])
-const wrap = ref(null)
+const wrap = ref<any>(null)
 
 onClickOutside(wrap, () => {
   emit('clickOutside')

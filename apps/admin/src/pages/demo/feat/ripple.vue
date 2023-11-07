d30079cb182d9cb773b84fcc3f356605f76e2734
huangxiaomin <1219549841@qq.com>
2023-11-07 11:18:26 +0800

    feat: add ripple demo

---
+++ apps/admin/src/pages/demo/feat/ripple.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+import { rippleDirective as vRipple } from '@vben/directives'
+</script>
+
+<template>
+  <VbenCard title="Ripple示例">
+    <div class="demo-box" v-ripple>content</div>
+  </VbenCard>
+</template>
+
+<style lang="less" scoped>
+.demo-box {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 300px;
+  height: 300px;
+  border-radius: 10px;
+  background-color: #408ede;
+  color: #fff;
+  font-size: 24px;
+}
+</style>
--- packages/directives/index.ts
+++ packages/directives/index.ts
@@ -1,1 +1,2 @@
 export { clickOutside } from './src/click-outside'
+export { rippleDirective } from './src/ripple/index'

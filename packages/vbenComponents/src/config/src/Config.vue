d2173453f8b15bc2b62e59b3d058bfadaea2aa24
likui628 <90845831+likui628@users.noreply.github.com>
2023-11-08 09:59:54 +0800

    generate colors

--- packages/vbenComponents/src/config/src/Config.vue
+++ packages/vbenComponents/src/config/src/Config.vue
@@ -3,6 +3,8 @@ import { maps } from '#/index'
 import { computed, unref } from 'vue'
 import { useAppTheme } from '@vben/hooks'
 import { ThemeEnum } from '@vben/constants'
+import { generate } from '@ant-design/colors'
+
 const Config = maps.get('Config')
 const darkTheme = maps.get('DarkTheme')
 
@@ -26,32 +28,58 @@ const appTheme = computed(() => {
   return unref(isDark) ? darkTheme : null
 })
 
-const themeOverrides = {
-  common: {
-    primaryColor: '#FF0000',
-  },
-  Button: {
-    textColor: '#FF0000',
-  },
-  Select: {
-    peers: {
-      InternalSelection: {
-        textColor: '#FF0000',
+const colorPrimary = '#2a64d4'
+const colorPrimaryList = generate(colorPrimary)
+const colorSuccess = '#52c41a'
+const colorSuccessList = generate(colorSuccess)
+const colorWarning = '#faad14'
+const colorWarningList = generate(colorWarning)
+const colorError = '#D03050'
+const colorErrorList = generate(colorError)
+const colorInfo = '#2080F0'
+const colorInfoList = generate(colorInfo)
+const colorNeutral = '#000000'
+
+function generateThemeOverrides() {
+  return {
+    themeOverrides: {
+      common: {
+        primaryColor: colorPrimaryList[5],
+        primaryColorHover: colorPrimaryList[4],
+        primaryColorPressed: colorPrimaryList[4],
+        primaryColorSuppl: colorPrimaryList[6],
+
+        infoColor: colorInfoList[5],
+        infoColorHover: colorInfoList[4],
+        infoColorPressed: colorInfoList[4],
+        infoColorSuppl: colorInfoList[6],
+
+        successColor: colorSuccessList[5],
+        successColorHover: colorSuccessList[4],
+        successColorPressed: colorSuccessList[4],
+        successColorSuppl: colorSuccessList[6],
+
+        warningColor: colorWarningList[5],
+        warningColorHover: colorWarningList[4],
+        warningColorPressed: colorWarningList[4],
+        warningColorSuppl: colorWarningList[6],
+
+        errorColorColor: colorErrorList[5],
+        errorColorHover: colorErrorList[4],
+        errorColorPressed: colorErrorList[4],
+        errorColorSuppl: colorErrorList[6],
+
+        // text color and so on ...
+        textColorBase: colorNeutral,
       },
     },
-  },
-  // ...
+  }
 }
+
+const theme = generateThemeOverrides()
 </script>
 <template>
-  <Config
-    v-bind="$attrs"
-    abstract
-    :theme="appTheme"
-    :theme-overrides="themeOverrides"
-  >
-    <slot></slot
-  ></Config>
+  <Config abstract :theme="appTheme" v-bind="theme"> <slot></slot></Config>
 </template>
 
 <style scoped></style>

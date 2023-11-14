901ac846e5458007c6242781648aee1e3082b182
likui628 <90845831+likui628@users.noreply.github.com>
2023-11-14 17:35:28 +0800

    chore(apps): remove preset config

--- apps/admin/vite.config.ts
+++ apps/admin/vite.config.ts
@@ -2,5 +2,5 @@ import { createViteConfig } from '@config/vite'
 import { defineConfig, UserConfig } from 'vite'
 
 export default defineConfig(async ({ command, mode }): Promise<UserConfig> => {
-  return await createViteConfig(command, mode, process.cwd(), {preset: 'antd'})
+  return await createViteConfig(command, mode, process.cwd())
 })

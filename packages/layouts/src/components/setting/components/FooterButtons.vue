5ef173621f269027b3639497fb87473ec2b7c71f
likui628 <90845831+likui628@users.noreply.github.com>
2023-11-17 14:57:13 +0800

    chore: remove the 'type' attribute of the settings button

--- packages/layouts/src/components/setting/components/FooterButtons.vue
+++ packages/layouts/src/components/setting/components/FooterButtons.vue
@@ -9,19 +9,19 @@ const { copyConfigs, resetAllConfig, clearAndRedo } = useAppConfig()
 </script>
 <template>
   <VbenSpace vertical>
-    <VbenButton type="info" block @click="copyConfigs">
+    <VbenButton block @click="copyConfigs">
       <template #icon>
         <VbenIconify icon="ant-design:snippets-twotone" />
       </template>
       {{ t('layout.setting.copyBtn') }}
     </VbenButton>
-    <VbenButton type="warning" @click="resetAllConfig" block>
+    <VbenButton @click="resetAllConfig" block>
       <template #icon>
         <VbenIconify icon="ant-design:reload-outlined" />
       </template>
       {{ t('common.resetText') }}
     </VbenButton>
-    <VbenButton type="error" block @click="clearAndRedo">
+    <VbenButton block @click="clearAndRedo">
       <template #icon>
         <VbenIconify icon="ant-design:redo-outlined" />
       </template>

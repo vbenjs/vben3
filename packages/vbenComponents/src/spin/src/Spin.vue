735437a7701c20b34bc22de9dbac988a53f8db67
jackhoo_98 <jackhoo_98@foxmail.com>
2023-11-25 20:24:31 +0800

    feat(vbenComponents): add Spin(Loading)

---
+++ packages/vbenComponents/src/spin/src/Spin.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup name="VbenSpin">
+import { maps } from '#/index'
+
+const Spin = maps.get('Spin')
+</script>
+<template>
+  <Spin v-bind="$attrs">
+    <template #[item]="data" v-for="item in Object.keys($slots)" :key="item">
+      <slot :name="item" v-bind="data || {}"></slot> </template
+  ></Spin>
+</template>
+
+<style scoped></style>

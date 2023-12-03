c1c40a61e7441c692a0958c3a566cbc76f010251
aonoa <32682251+aonoa@users.noreply.github.com>
2023-12-03 20:08:37 +0800

    feat(demo)：添加富文本组件示例(#147) (#285)
    * feat(demo): 添加富文本组件示例
    
    Signed-off-by: aonoa <1991849113@qq.com>
    
    * chore: 多提交了个文件
    
    Signed-off-by: aonoa <1991849113@qq.com>
    
    * fix: @tinymce/tinymce-vue
    
    ---------
    
    Signed-off-by: aonoa <1991849113@qq.com>
    Co-authored-by: Li Kui <90845831+likui628@users.noreply.github.com>

---
+++ apps/admin/src/pages/demo/editor/rich-text-editor.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+import { Editor } from '@vben/demo'
+import { ref } from "vue";
+
+let tinymceHtml = ref('<h1>hello world!</h1>')
+
+// 使用的时候自己去 https://www.tiny.cloud/auth/signup/ 申请一个key(免费的)，替换packages/demo/src/Editor/Editor.vue中的apiKey，不要用我这个
+
+function getRichContent(html: any) {
+  // 获取编辑器内容,内容变更时触发
+  // console.log(html)
+}
+</script>
+
+<template>
+    <VbenCard title="富文本组件示例"/>
+  <Editor :echo-data="tinymceHtml" @content="getRichContent" />
+</template>
+

862dcf2292a5cd5e8dd474701720a1706ca5b787
jinmao88 <50581550+jinmao88@users.noreply.github.com>
2023-12-16 07:40:48 +0800

    fix: 适配arcoDesgin

--- packages/vbenComponents/src/layout/src/LayoutSider.vue
+++ packages/vbenComponents/src/layout/src/LayoutSider.vue
@@ -5,7 +5,11 @@ import { maps } from '../../index'
 const LayoutSider = maps.get('LayoutSider')
 </script>
 <template>
-  <LayoutSider v-bind="$attrs">
+  <LayoutSider
+    v-bind="$attrs"
+    collapsible
+    :hide-trigger="!$attrs['show-trigger']"
+  >
     <template #[item]="data" v-for="item in Object.keys($slots)" :key="item">
       <slot :name="item" v-bind="data || {}"></slot> </template
   ></LayoutSider>
--- packages/vbenComponents/src/menu/src/Menu.vue
+++ packages/vbenComponents/src/menu/src/Menu.vue
@@ -5,9 +5,11 @@ import { ref } from 'vue'
 const Menu = maps.get('Menu')
 const Ref = ref(null)
 defineExpose({ Ref })
+const MenuItem = maps.get('MenuItem')
 </script>
 <template>
   <Menu v-bind="$attrs" ref="Ref">
+    <MenuItem v-for="i in $attrs.options" :key="i.key">{{ i.label }}</MenuItem>
     <template #[item]="data" v-for="item in Object.keys($slots)" :key="item">
       <slot :name="item" v-bind="data || {}"></slot> </template
   ></Menu>

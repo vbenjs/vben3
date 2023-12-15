9eb4b2aec852cbdbb35a73de1a0bac0415ac5b86
jinmao88 <50581550+jinmao88@users.noreply.github.com>
2023-12-15 12:51:21 +0800

    feat(button): 适配arcoDesign

--- packages/vbenComponents/src/button/src/Button.vue
+++ packages/vbenComponents/src/button/src/Button.vue
@@ -1,12 +1,13 @@
-<script lang="ts" setup name="VbenButton">
+<script lang="ts" setup>
 import { maps } from '../../index'
 import { ref } from 'vue'
+defineOptions({ name: 'VbenButton' })
 const Button = maps.get('Button')
 const Ref = ref(null)
 defineExpose({ Ref })
 </script>
 <template>
-  <Button v-bind="$attrs" ref="Ref">
+  <Button v-bind="$attrs" ref="Ref" long>
     <template #[item]="data" v-for="item in Object.keys($slots)" :key="item">
       <slot :name="item" v-bind="data || {}"></slot> </template
   ></Button>

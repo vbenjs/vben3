2bbf4aac81d7d8b09400c6c0306c92c80344c1ba
jinmao88 <50581550+jinmao88@users.noreply.github.com>
2024-02-11 18:47:48 +0800

    chore: mock使用提示

--- configs/vite/src/plugins/mock.ts
+++ configs/vite/src/plugins/mock.ts
@@ -7,8 +7,8 @@ import { vitePluginFakeServer } from 'vite-plugin-fake-server'
 // 使用新的依赖库 vite-plugin-fake-server
 // 注意事项：
 // 1. mock文件夹下，所有的 *.mock.ts 文件，都会被加载
-// 2. *.mock.ts 文件中 不能存在export default意外的export，抽离到别的ts即可，列如util
-// 3. *.mock.ts 文件中 只能import相对路径，即不可引入 '@vben/util/mock-util'
+// 2. *.mock.ts 文件中 不能存在export default以外的export，抽离到别的ts即可，列如util
+// 3. *.mock.ts 文件中 只能import相对路径，不可引入 '@vben/util/mock-util'
 
 export function configMockPlugin(isBuild: boolean) {
   return vitePluginFakeServer({
